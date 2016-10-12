a56838dabd33299ecfc43304d566679070193feb
Wolfram Huesken <wolfram.huesken@zalora.com>
2016-10-12 19:07:24 +0800

    Changed cache mode from replicated to distributed

--- src/main/java/com/zalora/config/CacheConfig.java
+++ src/main/java/com/zalora/config/CacheConfig.java
@@ -41,7 +41,7 @@ public class CacheConfig {
                 .clusterName(clusterName);
 
         configuration = new ConfigurationBuilder()
-            .clustering().cacheMode(CacheMode.REPL_ASYNC)
+            .clustering().cacheMode(CacheMode.DIST_ASYNC)
             .build();
 
         if (!isDev()) {
