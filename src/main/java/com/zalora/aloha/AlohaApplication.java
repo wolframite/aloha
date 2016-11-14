d43a03328c06aee21334e21fa8201b5b40dac480
Wolfram Huesken <wolfram.huesken@zalora.com>
2016-11-14 12:18:54 +0800

    Added H2 dependency to satisfy Spring on startup when no database is present

--- src/main/java/com/zalora/aloha/AlohaApplication.java
+++ src/main/java/com/zalora/aloha/AlohaApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AlohaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlohaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AlohaApplication.class, args);
+    }
+
 }
